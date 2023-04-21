2ed940378d9b170a2f26fceab2e971bbe9e308d7
Rumyantsev Andrew <wertyos111@gmail.com>
2023-04-22 03:59:42 +0600

    changed sqlite rows format

--- config.py
+++ config.py
@@ -2,7 +2,20 @@ from os import path
 from pathlib import Path
 from sqlite3 import Connection
 
-ROOT_DIR = path.dirname(path.abspath(__file__))
-DB_PATH = Path(ROOT_DIR) / 'activities_service_db.sqlite3'
 
-DB = Connection(DB_PATH).cursor()
+def __dict_factory(cursor, row):
+    d = {}
+    for idx, column in enumerate(cursor.description):
+        d[column[0]] = row[idx]
+    return d
+
+
+def __create_connection():
+    db_path = Path(ROOT_DIR) / 'activities_service_db.sqlite3'
+    connection = Connection(db_path, check_same_thread=False)
+    connection.row_factory = __dict_factory
+    return connection
+
+
+ROOT_DIR = path.dirname(path.abspath(__file__))
+DB = __create_connection()
