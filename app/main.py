a6f768ed6210bd2beac21ea936865bf46a6b9c0d
Rumyantsev Andrew <wertyos111@gmail.com>
2023-04-22 04:02:15 +0600

    added all activities and mock for creating

--- app/main.py
+++ app/main.py
@@ -1,5 +1,10 @@
+import datetime
+import random
+
 from fastapi import FastAPI
 from app.models.activity import ActivityList
+from app.services.get_activities_service import GetActivitiesService
+from app.queries.create_activity_query import CreateActivityQuery
 
 app = FastAPI()
 
@@ -11,5 +16,15 @@ def main_page():
 
 @app.get('/activities')
 def activities_list(query: str | None = None) -> ActivityList:
-    activities = ActivityList(activities=[], count=0)
-    return activities
+    activities = GetActivitiesService(query).perform()
+    return ActivityList(activities=activities, count=len(activities))
+
+
+# wwrong implementation just mock to easy data generate
+# TODO: remove
+@app.get('/make')
+def create_activity() -> dict:
+    attributes = {'title': 'hello', 'start_time': datetime.datetime(2023, 3, random.randint(1, 31))}
+    new = CreateActivityQuery(attributes)
+    new.execute()
+    return {'result': new.is_successful, 'error': new.error_message}
