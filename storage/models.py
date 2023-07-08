974a471833abed7b708839572f7feca0a30cb73b
deniska <diniskondratov-24@mail.ru>
2023-07-08 20:44:02 +0300

    added storage model

--- storage/models.py
+++ storage/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Storage(models.Model):
+    location = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    capacity = models.IntegerField(default=0)
+    created_at = models.DateTimeField()
