1f02c19ed5aa9d0b839b03b7175611705ba0878e
Rumyantsev Andrew <70966785+phantom-profile@users.noreply.github.com>
2023-07-09 00:14:57 +0600

    added simple view with params

--- storage/views.py
+++ storage/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django.http import HttpRequest
 
-# Create your views here.
+
+def index(request: HttpRequest):
+    if request.GET.get('error'):
+        return HttpResponse(content='You triggered error. Why??', status=400)
+
+    return HttpResponse(content='Hello, it is root page', status=200)
