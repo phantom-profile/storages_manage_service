96f769c6c27e60fbc757742fd2bd3e46bb6ed347
Rumyantsev Andrew <70966785+phantom-profile@users.noreply.github.com>
2023-07-09 00:03:16 +0600

    added data validations and defaults

--- storage/models.py
+++ storage/models.py
@@ -1,8 +1,12 @@
+from datetime import datetime
+
 from django.db import models
+# https://docs.djangoproject.com/en/4.2/ref/validators/ - validators docs
+from django.core.validators import MinValueValidator
 
 
 class Storage(models.Model):
-    location = models.CharField(max_length=200)
-    name = models.CharField(max_length=200)
-    capacity = models.IntegerField(default=0)
-    created_at = models.DateTimeField()
+    location = models.CharField(max_length=200, blank=False, null=False)
+    name = models.CharField(max_length=200, unique=True, blank=False, null=False)
+    capacity = models.IntegerField(validators=[MinValueValidator(1)], null=False)
+    created_at = models.DateTimeField(default=datetime.now, null=False)
