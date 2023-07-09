2167cf6eca45cf0284e79d72654e6d4136287439
Rumyantsev Andrew <70966785+phantom-profile@users.noreply.github.com>
2023-07-09 20:36:22 +0600

    added model task for today

--- storage/models.py
+++ storage/models.py
@@ -1,5 +1,4 @@
-from datetime import datetime
-
+from django.utils import timezone
 from django.db import models
 # https://docs.djangoproject.com/en/4.2/ref/validators/ - validators docs
 from django.core.validators import MinValueValidator
@@ -9,4 +8,13 @@ class Storage(models.Model):
     location = models.CharField(max_length=200, blank=False, null=False)
     name = models.CharField(max_length=200, unique=True, blank=False, null=False)
     capacity = models.IntegerField(validators=[MinValueValidator(1)], null=False)
-    created_at = models.DateTimeField(default=datetime.now, null=False)
+    created_at = models.DateTimeField(default=timezone.now, null=False)
+
+
+# model Truck
+# truck_id - unique string, uuid models.UUIDField with default call python uuid.uuid4
+# capacity - integer > 0
+# current_storage - 1toM relation with storage. One storage - many trucks. check models.ForeignKey
+# expluatation_start - datetime
+# expluatation_finish - datetime > expluatation_start
+# created_at - datetime, default = timezone.now
