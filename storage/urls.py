ac727d920b2dd3c3ea4169d950660e12c10d8b93
Rumyantsev Andrew <70966785+phantom-profile@users.noreply.github.com>
2023-07-16 16:47:30 +0300

    used url template tag instead of hardcode

--- storage/urls.py
+++ storage/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from storage import views
 
 urlpatterns = [
-    path('index', views.index, name='index'),
-    path('<int:storage_id>/detail', views.detail, name='detail'),
+    path('index', views.index, name='all-storages'),
+    path('<int:storage_id>/detail', views.detail, name='one-storage'),
     path('vote', views.vote, name='vote'),
-    path('results', views.results,name='results')
+    path('results', views.results, name='results')
 ]
