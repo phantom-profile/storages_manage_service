9e1b1b5e21470e3574196021f088d7c01ffd934a
denis <diniskondratov-24@mail.ru>
2023-07-22 23:51:41 +0300

    added new validator and updated field validating process

--- storage/forms.py
+++ storage/forms.py
@@ -1,7 +1,16 @@
 from django import forms
+from storage.models import Storage
+
+
+class UniqueValidator():
+    def __call__(self, name):
+        if Storage.objects.filter(name=name):
+            raise forms.ValidationError('ошибка')
 
 
 class StorageForm(forms.Form):
     location = forms.CharField(label='Storage location', max_length=100)
-    name = forms.CharField(label='Storage name', max_length=100)
+    name = forms.CharField(label='Storage name', max_length=100, validators=[UniqueValidator()])
     capacity = forms.IntegerField(label='Storage capacity')
+
+
