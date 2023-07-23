22d379885015036d913f438006319052c1549c6d
Rumyantsev Andrew <70966785+phantom-profile@users.noreply.github.com>
2023-07-23 21:38:10 +0300

    added state saver and integrated it

---
+++ lib/form_data_caching.py
@@ -0,0 +1,33 @@
+from json import dumps, loads
+from uuid import uuid4
+
+from lib.redis_client import Red
+from storage.forms import StorageForm
+
+
+class FormsFactory:
+    FORMS = {
+        'storage': StorageForm,
+    }
+
+    @classmethod
+    def save_state(cls, state: dict) -> str:
+        uuid = uuid4()
+        Red.setex(str(uuid), 60, dumps(state))
+        return str(uuid)
+
+    @classmethod
+    def restore_by_key(cls, key: str | None, form_type: str):
+        form_class = cls.FORMS.get(form_type)
+        if not form_class:
+            raise AttributeError(f'form with type {form_type} not registered')
+        if not key:
+            return form_class()
+        json_params = Red.get(key)
+        if not json_params:
+            return form_class()
+
+        params = loads(json_params)
+        form = form_class(params)
+        form.is_valid()
+        return form
--- storage/views.py
+++ storage/views.py
@@ -1,13 +1,16 @@
-from django.shortcuts import render, get_object_or_404, HttpResponse, redirect
+from django.shortcuts import render, get_object_or_404, HttpResponse, redirect, reverse
 from django.http import HttpRequest
 from django.db.models import Count
 
 from storage.models import Storage, Truck
 from storage.forms import StorageForm
+
 from lib.filter_storages_params import GetParamsFilter
+from lib.form_data_caching import FormsFactory
 
 
 def index(request: HttpRequest):
+    form = FormsFactory.restore_by_key(request.GET.get('storage_form_id'), 'storage')
     filter_service = GetParamsFilter(request.GET)
     if not filter_service.is_valid:
         return HttpResponse(content='invalid request params', status=422)
@@ -16,7 +19,7 @@ def index(request: HttpRequest):
     context = {
         'storage_list': storage_list,
         'change_to': filter_service.params['change_to'],
-        'form': StorageForm()
+        'form': form
     }
     return render(request, 'storage/index.html', context)
 
@@ -40,4 +43,7 @@ def create_storage(request: HttpRequest):
             capacity=form.cleaned_data.get("capacity")
         )
         storage.save()
-    return redirect(index)
+        return redirect(reverse('all-storages'))
+    else:
+        uuid = FormsFactory.save_state(request.POST)
+        return redirect(reverse('all-storages') + '?' + f"storage_form_id={uuid}")
