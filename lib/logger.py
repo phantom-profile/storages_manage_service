65fe1a9aad4db9ce630f03c63786b01204bdc4e7
denis <diniskondratov-24@mail.ru>
2023-07-26 23:54:02 +0300

    updated logger like in review again added log in gitignore

--- lib/logger.py
+++ lib/logger.py
@@ -1,16 +1,18 @@
 from storages_manage_service.settings import BASE_DIR
 from datetime import datetime
+from os import path, mkdir
 
 
 class WrongLogName(Exception):
     """The entered file name does not match the format (name.log)"""
 
 
-class DefaultLogger:
+class AppLogger:
+    LOG_DIR = BASE_DIR / "log"
+
     def __init__(self, log_name: str):
-        if not self.__correct_log_file_name(log_name):
-            raise WrongLogName("The entered file name does not match the format (name.log)")
-        self.log_path = BASE_DIR / "log" / log_name
+        self.__correct_log_file_name(log_name)
+        self.log_path = self.__create_path(log_name)
 
     def info(self, message: str, extra_params: dict = None):
         with open(self.log_path, "a") as log_file:
@@ -22,16 +24,21 @@ class DefaultLogger:
 
     @staticmethod
     def __correct_log_file_name(name: str):
-        split_string = name.split(".")
-        if len(split_string) == 2 and split_string[-1] == "log":
-            return True
+        if len(name.split(".")) != 2 or name.split(".")[-1] != "log":
+            raise WrongLogName("The entered file name does not match the format (name.log)")
 
     @staticmethod
     def __build_log_str(message: str = None, error: Exception = None, extra_params: dict = None) -> str:
         if message:
             return f"[{datetime.now()}] I, {message}, extra params = {extra_params}"
         if not error.args:
-            return f"[{datetime.now()}] W, catched error class - {type(error)}, information not provided, extra " \
-                   f"params = {extra_params}"
-        return f"[{datetime.now()}] W, catched error class - {type(error)}, info - {error.args}, extra " \
-               f"params = {extra_params}"
+            info = "information not provided"
+        else:
+            info = f"info - {error.args}"
+        return f"[{datetime.now()}] W, catched error class - {type(error)}, {info}, extra params = {extra_params}"
+
+    def __create_path(self, name: str):
+        if path.exists(self.LOG_DIR):
+            return self.LOG_DIR / name
+        mkdir(self.LOG_DIR)
+        return self.LOG_DIR / name
