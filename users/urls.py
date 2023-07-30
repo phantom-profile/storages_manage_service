c875606e8797c7b2c85a446ad1f6a792ebad5f8b
denis <diniskondratov-24@mail.ru>
2023-07-30 23:50:51 +0300

    fixed code like in review

--- users/urls.py
+++ users/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('register/', views.register, name='register'),
-    path('cards', views.index)
+    path('cards/', views.index, name='cards')
 ]
