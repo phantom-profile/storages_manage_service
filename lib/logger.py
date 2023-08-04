c018cfb94cb7a1f55c7af078037fc7a1a7515de5
Rumyantsev Andrew <70966785+phantom-profile@users.noreply.github.com>
2023-08-04 21:13:43 +0300

    refactored logger

--- lib/logger.py
+++ lib/logger.py
@@ -4,44 +4,49 @@ from os import path, mkdir
 
 
 class WrongLogName(Exception):
-    """The entered file name does not match the format (name.log)"""
+    MESSAGE = "The entered file name does not match the format (name.log)"
+
+    def __init__(self):
+        super().__init__(self.MESSAGE)
 
 
 class AppLogger:
+    EXTENSION = 'log'
     LOG_DIR = BASE_DIR / "log"
 
-    def __init__(self, log_name: str):
-        self.__correct_log_file_name(log_name)
-        self.log_path = self.__create_path(log_name)
+    def __init__(self, filename: str):
+        name_as_list = filename.split(".")
+        if len(name_as_list) != 2 or name_as_list[-1] != 'log':
+            raise WrongLogName()
+
+        self.filename = filename
+        self.params = None
 
     def info(self, message: str, params: dict = None):
-        with open(self.log_path, "a") as log_file:
-            print(self.__build_log_str(message=message, params=params), file=log_file)
+        self.params = params
+        with open(self.filepath, "a") as log_file:
+            print(self._string_template(f"I, {message}"), file=log_file)
 
     def warn(self, error: Exception, params: dict = None):
-        with open(self.log_path, "a") as log_file:
-            print(self.__build_log_str(error=error, params=params), file=log_file)
+        self.params = params
+        info = f"info - {error.args}" if error.args else "information not provided"
+        with open(self.filepath, "a") as log_file:
+            print(self._string_template(f"W, error - {type(error)}, {info}"), file=log_file)
 
-    @staticmethod
-    def __correct_log_file_name(name: str):
-        if len(name.split(".")) != 2 or name.split(".")[-1] != "log":
-            raise WrongLogName("The entered file name does not match the format (name.log)")
-
-    @staticmethod
-    def __build_log_str(message: str = None, error: Exception = None, params: dict = None) -> str:
-        if message:
-            return f"[{datetime.now()}] I, {message}, extra params = {params}"
-        if not error.args:
-            info = "information not provided"
-        else:
-            info = f"info - {error.args}"
-        return f"[{datetime.now()}] W, error - {type(error)}, {info}, extra params = {params}"
-
-    def __create_path(self, name: str):
+    @property
+    def filepath(self):
         if path.exists(self.LOG_DIR):
-            return self.LOG_DIR / name
+            return self.LOG_DIR / self.filename
         mkdir(self.LOG_DIR)
-        return self.LOG_DIR / name
+        return self.LOG_DIR / self.filename
+
+    def _validate_file_name(self, filename: str):
+        name_as_list = filename.split(".")
+        if len(name_as_list) != 2 or name_as_list[-1] != self.EXTENSION:
+            raise WrongLogName()
+
+    def _string_template(self, inserted: str):
+        return f"[{datetime.now()}] {inserted}, extra params = {self.params}"
 
 
 default_logger = AppLogger("log.log")
