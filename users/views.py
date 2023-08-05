a08131a5b722858ae209295b1984c2f715735d26
denis <diniskondratov-24@mail.ru>
2023-08-05 23:38:10 +0300

    created credit card form started creating views and templates for work payment client

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from uuid import uuid4
@@ -67,20 +67,25 @@ def index(request):
 def trust_card(request):
     form = FormsFactory.produce('credit-card', params=request.POST)
     if form.is_valid():
-        PayServiceClient().trust_card(
+        response = PayServiceClient().trust_card(
             card_number=form.cleaned_data.get("card_number"),
             cvv=form.cleaned_data.get("cvv"),
             owner=form.cleaned_data.get("owner"),
             bank=form.cleaned_data.get("bank"),
             vendor=form.cleaned_data.get("vendor")
         )
-        card = CreditCard(
-            card_number=form.cleaned_data.get("card_number"),
-            cvv=form.cleaned_data.get("cvv"),
-            owner=form.cleaned_data.get("owner"),
-            bank=form.cleaned_data.get("bank"),
-            vendor=form.cleaned_data.get("vendor"),
-            expires_at=form.cleaned_data.get("expires_at"),
-            card_uuid=uuid4()
-        )
+        if response["is_successful"]:
+            card = CreditCard(
+                card_number=form.cleaned_data.get("card_number"),
+                cvv=form.cleaned_data.get("cvv"),
+                owner=form.cleaned_data.get("owner"),
+                bank=form.cleaned_data.get("bank"),
+                vendor=form.cleaned_data.get("vendor"),
+                expires_at=form.cleaned_data.get("expires_at"),
+                card_uuid=response['response_body']['card_uuid']
+            )
+            card.save()
+            return redirect(reverse('cards'))
+        return redirect(reverse('cards'))
+    return redirect(reverse('cards'))
 
